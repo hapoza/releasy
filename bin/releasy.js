e9c4f66f695e897127852d988535fc44533d35d9
Dan <daniel.ddsc@gmail.com>
2018-08-23 14:33:00 -0300

    :sparkles: Make releasy accept prefix options for better usage with monorepos

--- bin/releasy.js
+++ bin/releasy.js
@@ -9,7 +9,11 @@ var optionsFile = steps.getOptionsFile()
 
 var type = optionsFile.type || 'patch'
 var args = process.argv
-if (['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(args[2]) !== -1) {
+if (
+  ['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(
+    args[2]
+  ) !== -1
+) {
   type = args[2]
   if (type === 'pre') type = 'prerelease'
   console.log('Release:', type)
@@ -17,7 +21,8 @@ if (['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(args[2])
   args = args.slice(0, 2).concat(args.slice(3))
 }
 
-program.version(pkg.version)
+program
+  .version(pkg.version)
   .usage('(major|minor|*patch*|prerelease) [options]')
   .option('-f, --filename [path]', 'Your package manifest file [package.json]')
   .option('-t, --tag-name [tag]', 'The prerelease tag in your version [beta]')
@@ -27,27 +32,34 @@ program.version(pkg.version)
   .option('--no-commit', 'Do not commit the version change')
   .option('--no-tag', 'Do not tag the version change')
   .option('--no-push', 'Do not push changes to remote')
-  .option('--notes', 'Publish notes to GitHub Release Notes. Personal Token is required to use this option')
+  .option(
+    '--notes',
+    'Publish notes to GitHub Release Notes. Personal Token is required to use this option'
+  )
   .option('-n, --npm', 'Publish to npm')
   .option('-d, --dry-run', 'Dont do anything, just show what would be done')
   .option('-s, --silent', 'Dont ask for confirmation')
   .option('-q, --quiet', "Don't write messages to console")
+  .option('--msg-prefix [msg-prefix]', 'Prepends text to commit messages')
+  .option('--tag-prefix [tag-prefix]', 'Prepends text to tags')
   .parse(args)
 
 var defaults = {
-  'filename': 'package.json',
+  filename: 'package.json',
   'tag-name': 'beta',
   'npm-tag': '',
-  'folder': '',
-  'stable': false,
+  folder: '',
+  stable: false,
   'no-commit': false,
   'no-tag': false,
   'no-push': false,
-  'notes': false,
-  'npm': false,
+  notes: false,
+  npm: false,
   'dry-run': false,
-  'silent': false,
-  'quiet': false,
+  silent: false,
+  quiet: false,
+  'msg-prefix': '',
+  'tag-prefix': ''
 }
 
 for (const key in defaults) {
